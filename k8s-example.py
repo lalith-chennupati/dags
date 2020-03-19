c585d92028fbc30af226dd02e6d630c65cb1a74b
lalith-chennupati <56936664+lalith-chennupati@users.noreply.github.com>
2020-03-19 06:13:19 -0700

    updated namespace in the dag

--- k8s-example.py
+++ k8s-example.py
@@ -1,44 +1,51 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow import configuration as conf
+from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2019, 1, 1),
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=5)
 }
 
-namespace = conf.get('kubernetes', 'NAMESPACE')
-
-# This will detect the default namespace locally and read the 
-# environment namespace when deployed to Astronomer.
-if namespace =='default':
-    config_file = '/usr/local/airflow/include/.kube/config'
-    in_cluster=False
-else:
-    in_cluster=True
-    config_file=None
-
-dag = DAG('example_kubernetes_pod',
-          schedule_interval='@once',
-          default_args=default_args)
 
 
+dag = DAG('kubernetes_sample', description='Simple kubernetes sample DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
 
-with dag:
-    k = KubernetesPodOperator(
-        namespace=s3data,
-        image="hello-world",
-        labels={"foo": "bar"},
-        name="airflow-test-pod",
-        task_id="task-one",
-        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
-        cluster_context='eks-test-cluster', # is ignored when in_cluster is set to True
-        config_file=config_file,
-        is_delete_operator_pod=True,
-        get_logs=True)
+start = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
+
+passing = KubernetesPodOperator(namespace='airflow',
+                          image="python:3.6",
+                          cmds=["python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="passing-test",
+                          task_id="passing-task",
+                          get_logs=True,
+                          dag=dag,
+                          in_cluster=True
+                          )
+
+failing = KubernetesPodOperator(namespace='airflow',
+                          image="ubuntu:16.04",
+                          cmds=["python","-c"],
+                          arguments=["print('hello world')"],
+                          labels={"foo": "bar"},
+                          name="fail",
+                          task_id="failing-task",
+                          get_logs=True,
+                          dag=dag,
+                          in_cluster=True
+                          )
+#start >> passing >> failing
+
+passing.set_upstream(start)
+failing.set_upstream(start)
